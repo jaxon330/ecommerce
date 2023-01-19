e52688c59557cec70a6b6c030a08645e2b005eee
john-330 <jaxon330@gmail.com>
2023-01-18 22:49:50 -0600

    created shipping options

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,9 +1,20 @@
+import Stripe from 'stripe';
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+
   if (req.method === 'POST') {
+    console.log(req.body)
     try {
         const params = {
+            submit_type: 'pay',
+            mode: 'payment',
+            payment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options: [
+                { shipping_rate: 'shr_1MRokhHKvM6SUfqEzKT718Kk' },
+                { shipping_rate: 'shr_1MRom1HKvM6SUfqE06fwDqc0' }
+            ],
             line_items: [
               {
                 // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
