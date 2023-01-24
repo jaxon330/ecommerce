0aa3620235fe44fd79201398df683b7b96ddacb2
john-330 <jaxon330@gmail.com>
2023-01-23 20:42:18 -0600

    Modified Footer.js and Navbar.js

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -9,7 +9,6 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
       <div className='banner-desc'>
         <div className='left'>
           <p>{discount}</p>
-          <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className='navbar-container'>
       <p className='logo'>
         <Link href="/">
-          Jakhongir's Headphones
+          Media Market
         </Link>
       </p>
 
